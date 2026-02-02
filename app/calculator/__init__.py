8bc69c8c4e6b2b510e3ea8f493c716386acc72e1
Matthew Belanger <msb64@njit.edu>
2026-02-01 19:20:46 -0500

    Create my own app files

---
+++ app/calculator/__init__.py
@@ -0,0 +1,47 @@
+from app.operations import addition, division, multiplication, subtraction
+
+def calculator():
+
+    print('Welcome to the REPL calculator!')
+
+    # Keep prompting the user for calculations until they type 'exit'
+    while True:
+
+        user_input = input('Enter an operation (add, subtract, multiply, divide), followed by two numbers. Enter "exit" to quit:').strip().lower()
+
+        if user_input == 'exit':
+            print('exiting calculator')
+            break
+
+        try:
+
+            # Extract the parts of the user input (operation and 2 numbers)
+            parts = user_input.split()
+            if len(parts) != 3:
+                print('Invalid input: Please follow the format: <operation> <number> <number>')
+            operation = parts[0]
+            num_1 = float(parts[1])
+            num_2 = float(parts[2])
+
+            # Do the operation
+            if operation == 'add':
+                result = addition(num_1, num_2)
+            elif operation == 'subtract':
+                result = subtraction(num_1, num_2)
+            elif operation == 'multiply':
+                result = multiplication(num_1, num_2)
+            elif operation == 'divide':
+                try:
+                    result = division(num_1, num_2)
+                except ValueError as e:
+                    print(e)
+                    continue
+            else:
+                print('Unknown operation: Please enter: add, subtract, multiply, or divide')
+                continue
+
+        except Exception as e:
+            print('Invalid input. Please follow the format: <operation> <number> <number>')
+            continue
+
+        print(f'Result: {result}')
---
+++ app/operations/__init__.py
@@ -0,0 +1,22 @@
+def addition(a: float, b: float) -> float:
+
+    # Add a and b
+    return a + b
+
+def subtraction(a: float, b: float) -> float:
+
+    # Subtract a and b
+    return a - b
+
+def multiplication(a: float, b: float) -> float:
+
+    # Multiply a and b
+    return a * b
+
+def division(a: float, b: float) -> float:
+
+    # Divide a by b. Raise an error if b is 0.
+    if b == 0:
+        raise ValueError('Division by zero is not allowed.')
+
+    return a / b
